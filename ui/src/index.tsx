93d317134617819d858b1ce5f3601ab52b41d560
Satya Kiran <satya.rayasam@opsmx.io>
2022-10-12 15:53:00 +0530

    initial commit

---
+++ ui/src/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+interface ApplicationResourceTree {}
+interface State {}
+
+export const Extension = (props: {
+  tree: ApplicationResourceTree;
+  resource: State;
+}) => {
+  return (
+    <div className="shopping-list">
+      <h1>Shopping List</h1>
+      <ul>
+        <li>Instagram</li>
+        <li>WhatsApp</li>
+        <li>Oculus</li>
+      </ul>
+    </div>
+  );
+};
+
+export const component = Extension;
---
+++ ui/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const groupKind = 'argoproj.io/ISDSample';
+
+const config = {
+  entry: {
+    extension: './src/index.tsx',
+  },
+  output: {
+    filename: 'extensions.js',
+    path: __dirname + `/dist/resources/${groupKind}/ui`,
+    libraryTarget: 'window',
+    library: ['extensions', 'resources', groupKind],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.ttf'],
+  },
+  externals: {
+    react: 'React',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          allowTsInNodeModules: true,
+          configFile: path.resolve('./src/tsconfig.json')
+        },
+      },
+      {
+        // prevent overriding global page styles
+        test: path.resolve(__dirname, 'node_modules/argo-ui/src/components/page/page.scss'),
+        use: 'null-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'raw-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'raw-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = config;
